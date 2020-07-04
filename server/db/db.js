fc22d423233913683d71fdcc597fcefc2185ab76
Kanishq <kanishq@spennyapp.com>
2020-07-04 18:15:17 +0530

    boiler plate with atlas

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
+const uri =
+  'mongodb+srv://kanishq:kanishq@serveitbackend.ovsub.mongodb.net/serveIt?retryWrites=true&w=majority';
 mongoose
-  .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/testNames', {
+  .connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => {
     console.log('Successfully connected to the MONGODB');
   })
-  .catch(err => {
+  .catch((err) => {
     console.log('Could not connect to the MONGODB. Exiting now...', err);
     process.exit();
   });
--- server/routes/test.js
+++ server/routes/test.js
@@ -4,28 +4,30 @@ const Name = require('../models/test');
 const router = express.Router();
 
 /* GET all Names */
-router.get('/allNames', async function(req, res, next) {
+router.get('/allNames', async function (req, res, next) {
   try {
     const names = await Name.find();
     res.status(200).send(names);
+    // console.log(res);
   } catch (err) {
     res.status(500).send({
       success: false,
-      message: err.message
+      message: err.message,
     });
   }
 });
 
 /* Add new Name */
-router.post('/addName', async function(req, res, next) {
+router.post('/addName', async function (req, res, next) {
   try {
     const name = new Name(req.body);
     await name.save();
+    console.log(res);
     res.status(200).send(name);
   } catch (err) {
     res.status(500).send({
       success: false,
-      message: err.message
+      message: err.message,
     });
   }
 });
